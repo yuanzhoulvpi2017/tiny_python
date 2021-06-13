570ee24d73368d0f845823b8bcd9373dba100a6d
yuanz <yuanzhoulvpi@outlook.com>
2021-06-13 23:53:05 +0800

    add first simple code

---
+++ multi_process/simple_01.py
@@ -0,0 +1,20 @@
+import time
+from tqdm import tqdm
+
+
+def myf(x):
+    time.sleep(1)
+    return x * x
+
+
+if __name__ == '__main__':
+
+    listx = range(10)
+    listy = []
+
+    for i in tqdm(listx):
+        temp_value = myf(x=i)
+        listy.append(temp_value)
+
+print('final y:')
+print(listy)
