3cbfe1fa9b1dde4bf22867c0ec3158b5993c5f1e
yuanz <yuanzhoulvpi@outlook.com>
2021-06-14 08:35:43 +0800

    share data in multiprocess

--- multi_process/06_multiwritedata.py
+++ multi_process/06_multiwritedata.py
@@ -3,7 +3,8 @@
 from multiprocessing import Process, Manager
 import os
 import time
-import pandas as pd 
+import pandas as pd
+from tqdm import tqdm
 
 def worker(id, save_data):
     time.sleep(1)
@@ -19,13 +20,13 @@ if __name__ == "__main__":
     
     subprocess_list = []
 
-    for i in range(10):
+    for i in tqdm(range(200)):
         p = Process(target=worker, args=(i, finaldata))
         subprocess_list.append(p)
         p.start()
 
 
-    [p.join() for p in subprocess_list]
+    [p.join() for p in tqdm(subprocess_list)]
 
     finaldata = pd.concat([pd.DataFrame(value) for (key, value) in finaldata.items()])
 
